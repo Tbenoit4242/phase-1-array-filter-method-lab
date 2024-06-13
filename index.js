7ec158b237bee1b2b4cde74e8ad8621760ab4f9a
Trevor Benoit <Trevorb042@gmail.com>
2024-06-13 10:13:31 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+// findMatching()
+// 1) returns all drivers that match the passed in name
+// 2) returns matching drivers if case does not match but letters do
+// 3) returns an empty array if there is no match
+function findMatching(drivers, query) {
+    return drivers.filter(driver => {
+      
+      const lowercasedDriver = driver.toLowerCase();
+      const lowercasedQuery = query.toLowerCase();
+      
+      return lowercasedDriver === lowercasedQuery;
+    });
+  }
+  function fuzzyMatch(drivers, query) {
+    
+    const lowercasedQuery = query.toLowerCase();
+    
+    
+    return drivers.filter(driver => {
+   
+      const lowercasedDriver = driver.toLowerCase();
+      
+  
+      return lowercasedDriver.startsWith(lowercasedQuery);
+    });
+  }
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
